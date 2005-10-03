bb53b609ac118ee4d472f8f8d8ed99465b53543b
(no author) <(no author)@5bbd4a9e-d125-0410-bf1d-f987e7eefc80>
2005-10-03 16:32:37 +0000

    This commit was manufactured by cvs2svn to create tag 'GLIB_2_8_3'.
    git-svn-id: svn+ssh://svn.gnome.org/svn/glib/tags/GLIB_2_8_3@4293 5bbd4a9e-d125-0410-bf1d-f987e7eefc80

--- glib/gkeyfile.c
+++ glib/gkeyfile.c
@@ -355,7 +355,7 @@ g_key_file_load_from_fd (GKeyFile       *key_file,
 			 GError        **error)
 {
   GError *key_file_error = NULL;
-  gsize bytes_read;
+  gssize bytes_read;
   struct stat stat_buf;
   gchar read_buf[4096];
   
@@ -2989,14 +2989,9 @@ g_key_file_line_is_group (const gchar *line)
 
   p = g_utf8_next_char (p);
 
-  if (!*p)
-    return FALSE;
-
-  p = g_utf8_next_char (p);
-
   /* Group name must be non-empty
    */
-  if (*p == ']')
+  if (!*p || *p == ']')
     return FALSE;
 
   while (*p && *p != ']')
